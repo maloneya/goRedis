2228816ee58be1489baaaf9c48c7c2eb56a36cff
Alex Maloney <maloney.a12@gmail.com>
2020-10-10 23:50:13 -0700

    Simplify potentially unnecessary work queues

--- main.go
+++ main.go
@@ -5,41 +5,19 @@ import (
 	"net/http"
 )
 
-type Request struct {
-	Key    string
-	Result chan string
-}
-
-func (r Request) String() string {
-	return fmt.Sprintln(r.Key)
-}
-
-func get(work_queue chan Request) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hi!")
-		//todo error handling
-		keys, _ := r.URL.Query()["key"]
-		if len(keys) > 0 {
-			req := Request{keys[0], make(chan string)}
-			work_queue <- req
-			res := <-req.Result
-			fmt.Printf("%v\n", res)
-		}
-	}
-}
-
-func RequestConsuemr(c <-chan Request, p Provider) {
-	for request := range c {
-		result := p.fetchKey(request.Key)
-		request.Result <- result
+func (provider RedisProvider) get(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hi!")
+	//todo error handling
+	keys, _ := r.URL.Query()["key"]
+	if len(keys) > 0 {
+		res := provider.fetchKey(keys[0])
+		fmt.Printf("%v\n", res)
 	}
 }
 
 func main() {
 	p := RedisProvider{RedisClientWrapper{}, LRUCache{make(map[string]string)}}
-	c := make(chan Request)
 
-	go RequestConsuemr(c, p)
-	http.HandleFunc("/get", get(c))
+	http.HandleFunc("/get", p.get)
 	http.ListenAndServe(":8090", nil)
 }
